27559fdcf03c10633b1bbc74640092f4e95f5bdd
ajimix <ajimix@gmail.com>
2013-04-19 16:41:21 +0200

    Improved readability

--- assets/js/background.js
+++ assets/js/background.js
@@ -1,11 +1,11 @@
 // Add listener for all ajax request so we can localize the changed strings
 
-chrome.webRequest.onCompleted.addListener(function(details) {
-    chrome.tabs.getSelected(null, function(tab) {
-		chrome.tabs.sendRequest(tab.id, {}, function(){});
-	});
+chrome.webRequest.onCompleted.addListener( function( details ) {
+	chrome.tabs.getSelected( null, function( tab ) {
+		chrome.tabs.sendRequest( tab.id, {}, function(){} );
+	} );
 }, {
 	urls: [
-      "*://app.asana.com/*"
-    ]
+		"*://app.asana.com/*"
+	]
 });
