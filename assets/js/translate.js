616d5ceaa92665e36d8fe5a1c50e30c9a99a7502
ajimix <ajimix@gmail.com>
2013-04-26 11:48:40 +0200

    Improved readability

--- assets/js/translate.js
+++ assets/js/translate.js
@@ -1,21 +1,36 @@
 /**
-*
-* Extension to translate ASANA.
-*
-* This script file is injected into ASANA and translate function it's called each time ASANA does an XMLHttpRequest translating string of the site
-*
-* Licensed under the Apache License 2.0
-*
-* Author: Ajimix [github.com/ajimix]
-*
-*/
-var stringToElement = function(element, string, property){ // Quick function to change text
-		if(typeOf(property) === 'null'){ property = 'text'; } // By default we change text, but we can also change for example placeholders (for inputs)
+ *
+ * Extension to translate ASANA.
+ *
+ * This script file is injected into ASANA and translate function it's called each time ASANA does an XMLHttpRequest translating string of the site
+ *
+ * Licensed under the Apache License 2.0
+ *
+ * Author: Ajimix [github.com/ajimix]
+ *
+ */
 
-		if(instanceOf(element, Element) || (instanceOf(element, Elements) && element.length > 0)){ // We only update the text if we found the element
-			var newValue = chrome.i18n.getMessage(string);
-			if (newValue){ // If empty then no change
-				element.set(property, newValue);
+( function ( doc, _UNDEFINED_ ) {
+
+	'use strict';
+
+	/**
+	 * Quick function to change text
+	 * @param  {object} element The element to process.
+	 * @param  {string} string String to change.
+	 * @param  {string} property Property to change. Text by default.
+	 * @return {Boolean} If property has been changed correctly or not.
+	 */
+var stringToElement = function ( element, string, property ) {
+		// By default we change text, but we can also change for example placeholders (for inputs)
+		if ( property === _UNDEFINED_ ) {
+			property = 'text';
+		}
+
+		if( instanceOf( element, Element ) || ( instanceOf( element, Elements ) && element.length > 0 ) ) { // We only update the text if we found the element
+			var newValue = chrome.i18n.getMessage( string );
+			if ( newValue ){ // If empty then no change
+				element.set( property, newValue );
 			}
 			return true;
 		}
@@ -26,29 +41,29 @@ var stringToElement = function(element, string, property){ // Quick function to 
 	 * Changes the text of the elements replacing the occurrences of the json data.
 	 * Use this for multiple strings in the same element or for elements that have children and don't have a good way to refer to the element to change.
 	 *
-	 * @param json strings {'text to find': 'keyword_from_strings_file', ...}
+	 * @param json strings { 'text to find': 'keyword_from_strings_file', ...}
 	 **/
-	replaceFromElement = function(element, strings){
-		element = $$(element);
-		if(element.length > 0 && instanceOf(element, Elements)){
-			element.each(function(el){ // Do the same for each element in array of elements
-				var newHtml = el.get('html');
-				Object.each(strings, function(item, key){
-					if(newHtml.indexOf(key) !== -1){
-						newHtml = newHtml.replace(key, chrome.i18n.getMessage(item));
+	replaceFromElement = function ( element, strings ) {
+		element = $$( element );
+		if ( element.length > 0 && instanceOf( element, Elements ) ) {
+			element.each( function( el ) { // Do the same for each element in array of elements
+				var newHtml = el.get( 'html' );
+				Object.each( strings, function( item, key ) {
+					if( newHtml.indexOf( key ) !== -1 ) {
+						newHtml = newHtml.replace( key, chrome.i18n.getMessage( item ) );
 					}
-				});
-				if (newHtml){ // If empty then no change
-					el.set('html', newHtml);
+				} );
+				if ( newHtml ) { // If empty then no change
+					el.set( 'html', newHtml ) ;
 				}
-			});
+			} );
 			return true;
 		}
 		return false;
 	},
 
 	// Translates strings
-	translate = function(){
+	translate = function () {
 		var days = {
 			'Today': 'Today',
 			'Yesterday': 'Yesterday',
@@ -62,80 +77,80 @@ var stringToElement = function(element, string, property){ // Quick function to 
 			'Sunday': 'Sunday'
 		};
 
-		console.time("Asana translate:");
+		console.time( "Asana translate:" );
 
 		// Try to not refer direct > child as Asana could change element structure
 
 		// Left menu
-		stringToElement($('nav_search_input'), 'Search', 'placeholder');
-		replaceFromElement($$('.atm-name'), {
+		stringToElement( $( 'nav_search_input' ), 'Search', 'placeholder' );
+		replaceFromElement( $$( '.atm-name' ), {
 			'My Tasks': 'MyTasks',
 			'My': 'My_Tasks',
 			'Tasks': 'Tasks'
 		});
-		stringToElement($$('.list-item.feed .list-item-caption'), 'Inbox');
-		stringToElement($$('#list_navigation_view__new_pot_button .button-text'), 'NewProject');
-		stringToElement($('manage_members'), 'InviteManageMembers');
-		stringToElement($('edit_workspace_settings'), 'EditWorkspaceSettings');
-		stringToElement($('remove_self'), 'RemoveMeFromWorkspace');
-		stringToElement($('upgrade_workspace'), 'UpgradeWorkspace');
-		replaceFromElement($('toggle_show_archived_projects'), {'Show Archived Projects': 'ShowArchivedProjects', 'Hide Archived Projects': 'HideArchivedProjects'});
-		stringToElement($('user_menu_account_settings'), 'AccountSettings');
-		stringToElement($('user_menu_new_workspace'), 'NewWorkspace');
-		stringToElement($('user_menu_logout'), 'LogOut');
-		stringToElement($$('.workspace-link'), 'ViewProjectsTagsPeople');
-		stringToElement($$('#add_project_button .button-text'), 'LEFT_NewProject');
-		stringToElement($$('#add_person_button .button-text'), 'AddPerson');
-		replaceFromElement($$('#navigation_dock_header .title-text'), { 'Workspace Overview': 'WorkspaceOverview' });
-		stringToElement($$('#new_workspace'), 'LEFT_NewWorkspace');
-		//replaceFromElement($$('#navigation_dock_workspace'), { 'Inbox': 'Inbox' });  // Crashes ASANA
+		stringToElement( $$( '.list-item.feed .list-item-caption' ), 'Inbox' );
+		stringToElement( $$( '#list_navigation_view__new_pot_button .button-text' ), 'NewProject' );
+		stringToElement( $( 'manage_members' ), 'InviteManageMembers' );
+		stringToElement( $( 'edit_workspace_settings' ), 'EditWorkspaceSettings' );
+		stringToElement( $( 'remove_self' ), 'RemoveMeFromWorkspace' );
+		stringToElement( $( 'upgrade_workspace' ), 'UpgradeWorkspace' );
+		replaceFromElement( $( 'toggle_show_archived_projects' ), { 'Show Archived Projects': 'ShowArchivedProjects', 'Hide Archived Projects': 'HideArchivedProjects' } );
+		stringToElement( $( 'user_menu_account_settings' ), 'AccountSettings' );
+		stringToElement( $( 'user_menu_new_workspace' ), 'NewWorkspace' );
+		stringToElement( $( 'user_menu_logout' ), 'LogOut' );
+		stringToElement( $$( '.workspace-link' ), 'ViewProjectsTagsPeople' );
+		stringToElement( $$( '#add_project_button .button-text' ), 'LEFT_NewProject' );
+		stringToElement( $$( '#add_person_button .button-text' ), 'AddPerson' );
+		replaceFromElement( $$( '#navigation_dock_header .title-text' ), { 'Workspace Overview': 'WorkspaceOverview' } );
+		stringToElement( $$( '#new_workspace' ), 'LEFT_NewWorkspace' );
+		//replaceFromElement( $$( '#navigation_dock_workspace' ), { 'Inbox': 'Inbox' });  // Crashes ASANA
 
 		// Left tabs under menu
-		stringToElement($$('.projects_tab'), 'PROJECTS');
-		stringToElement($$('.tags_tab'), 'TAGS');
-		stringToElement($$('.people_tab'), 'PEOPLE');
+		stringToElement( $$( '.projects_tab' ), 'PROJECTS' );
+		stringToElement( $$( '.tags_tab' ), 'TAGS' );
+		stringToElement( $$( '.people_tab' ), 'PEOPLE' );
 
 		// Left menu bottom
-		// replaceFromElement($$('#footer'), { 'Feedback': 'Feedback' });  // Crashes ASANA
-		stringToElement($$('#about_menu'), 'About');
+		// replaceFromElement( $$( '#footer' ), { 'Feedback': 'Feedback' });  // Crashes ASANA
+		stringToElement( $$( '#about_menu' ), 'About' );
 
 		// Task info
-		replaceFromElement($$('.complete-text'), { 'Mark Complete': 'MarkComplete', 'Mark Incomplete': 'MarkIncomplete', 'Completed': 'Completed' });
-		stringToElement($$('.description .placeholder-content'), 'AddNotes');
-		stringToElement($$('.assigned_to .property-name'), 'Assignee');
-		stringToElement($$('.projects .property-name'), 'Projects');
-		stringToElement($$('.projects .placeholder'), 'AddToProject');
-		if(!stringToElement($$('.due_date.property-name'), 'DueDate')){ // This is for assigned due dates
-			stringToElement($$('.due_date .property-name'), 'DueDate'); // Unassigned ones
+		replaceFromElement( $$( '.complete-text' ), { 'Mark Complete': 'MarkComplete', 'Mark Incomplete': 'MarkIncomplete', 'Completed': 'Completed' } );
+		stringToElement( $$( '.description .placeholder-content' ), 'AddNotes' );
+		stringToElement( $$( '.assigned_to .property-name' ), 'Assignee' );
+		stringToElement( $$( '.projects .property-name' ), 'Projects' );
+		stringToElement( $$( '.projects .placeholder' ), 'AddToProject' );
+		if( !stringToElement( $$( '.due_date.property-name' ), 'DueDate' ) ) { // This is for assigned due dates
+			stringToElement( $$( '.due_date .property-name' ), 'DueDate' ); // Unassigned ones
 		}
-		if(!stringToElement($$('.tags.property-name'), 'Tags')){ // This is for assigned tags
-			stringToElement($$('.tags .property-name'), 'Tags'); // Unassigned ones
+		if( !stringToElement( $$( '.tags.property-name' ), 'Tags' ) ) { // This is for assigned tags
+			stringToElement( $$( '.tags .property-name' ), 'Tags' ); // Unassigned ones
 		}
-		stringToElement($$('.attach-label'), 'AttachFile');
-		stringToElement($$('.followers_du .property-name'), 'Followers');
-		stringToElement($$('.followers_du .placeholder'), 'AddFollowers');
-		replaceFromElement($$('.follow-button .button-text'), {'Follow': 'Follow', 'Unfollow': 'Unfollow'});
-		stringToElement($$('.status-toggle-group .status-text')[0], 'Today');
-		stringToElement($$('.status-toggle-group .status-text')[1], 'Upcoming');
-		stringToElement($$('.status-toggle-group .status-text')[2], 'Later');
-		stringToElement($('delete_items'), 'DeleteTask');
-		stringToElement($('make_priority_heading'), 'MakePriorityHeading');
-		stringToElement($$('.details-pane-title .header-name')[2], 'NewItem', 'placeholder');
-		stringToElement($$('.assign-to-me-button .button-text'), 'AssignToMe');
-		stringToElement($$('.assigned_to .placeholder'), 'AssignToTeammate');
-		stringToElement($$('.comments .section-name'), 'ActivityFeed');
-		stringToElement($$('.comment-placeholder'), 'Comment');
+		stringToElement( $$( '.attach-label' ), 'AttachFile' );
+		stringToElement( $$( '.followers_du .property-name' ), 'Followers' );
+		stringToElement( $$( '.followers_du .placeholder' ), 'AddFollowers' );
+		replaceFromElement( $$( '.follow-button .button-text' ), { 'Follow': 'Follow', 'Unfollow': 'Unfollow' } );
+		stringToElement( $$( '.status-toggle-group .status-text' )[0], 'Today' );
+		stringToElement( $$( '.status-toggle-group .status-text' )[1], 'Upcoming' );
+		stringToElement( $$( '.status-toggle-group .status-text' )[2], 'Later' );
+		stringToElement( $( 'delete_items' ), 'DeleteTask' );
+		stringToElement( $( 'make_priority_heading' ), 'MakePriorityHeading' );
+		stringToElement( $$( '.details-pane-title .header-name' )[2], 'NewItem', 'placeholder' );
+		stringToElement( $$( '.assign-to-me-button .button-text' ), 'AssignToMe' );
+		stringToElement( $$( '.assigned_to .placeholder' ), 'AssignToTeammate' );
+		stringToElement( $$( '.comments .section-name' ), 'ActivityFeed' );
+		stringToElement( $$( '.comment-placeholder' ), 'Comment' );
 
-		stringToElement($$('#ical'), 'ICal');
-		stringToElement($$('#print'), 'Print');
-		stringToElement($$('#duplicate_project'), 'DuplicateProject');
-		stringToElement($$('#set_archived_pot'), 'ArchiveProject');
-		stringToElement($$('#delete_pot'), 'DeleteProject');
-		stringToElement($$('#convert_pot'), 'ConvertProjectToTag');
-		stringToElement($$('#details_property_sheet__new_comment_button .button-text'), 'BUTTON_Comment');
+		stringToElement( $$( '#ical' ), 'ICal' );
+		stringToElement( $$( '#print' ), 'Print' );
+		stringToElement( $$( '#duplicate_project' ), 'DuplicateProject' );
+		stringToElement( $$( '#set_archived_pot' ), 'ArchiveProject' );
+		stringToElement( $$( '#delete_pot' ), 'DeleteProject' );
+		stringToElement( $$( '#convert_pot' ), 'ConvertProjectToTag' );
+		stringToElement( $$( '#details_property_sheet__new_comment_button .button-text' ), 'BUTTON_Comment' );
 
 		// Task activity
-		replaceFromElement($$('.comments .feed-story .comment-text'), {
+		replaceFromElement( $$( '.comments .feed-story .comment-text' ), {
 			'created task': 'ACT_CreatedTask',
 			'added to': 'ACT_AddedTo',
 			'assigned to': 'ACT_AssignedTo',
@@ -151,36 +166,36 @@ var stringToElement = function(element, string, property){ // Quick function to 
 		});
 
 		// Project tasks
-		// replaceFromElement($$('.upcoming_group .group_header'), {'Upcoming': 'Upcoming'}); // Crashes ASANA
-		// replaceFromElement($$('.today_group .group_header'), {'Today': 'Today'}); // Crashes ASANA
-		stringToElement($$('.all-my-tasks-bar .label'), 'LATER');
-		replaceFromElement($$('.grid_due_date'), days);
-		stringToElement($$('.grid_pseudorow_no_prioritized .grid_cell_string'), 'AddTask');
-		stringToElement($$('#archive_menu .button-text'), 'Archive');
-		stringToElement($('archive_menu_item'), 'ArchiveCompletedTasks');
-		replaceFromElement($('toggle_archived_menu_item'), {'Show archived tasks': 'ShowArchivedTasks', 'Hide archived tasks': 'HideArchivedTasks'});
-		stringToElement($$('#new_menu .button-text'), 'New');
-		stringToElement($('new_menu_item_new_task'), 'NewTask');
-		stringToElement($('new_menu_item_new_priority_heading'), 'NewPriorityHeading');
-		stringToElement($$('#project_share_button .button-text'), 'Share');
-		replaceFromElement($$('.collapse-expand-all .button-text'), {'Collapse All': 'CollapseAll', 'Expand All': 'ExpandAll'});
-		stringToElement($$('#group_by_priority .button-text'), 'Priority');
-		stringToElement($$('#group_by_assignee .button-text'), 'Assignee');
-		stringToElement($$('#group_by_due_date .button-text'), 'Date');
-		stringToElement($$('#group_by_project .button-text'), 'Project');
-		stringToElement($$('.filter-buttons > .toggle-button > .button-text'), 'All');
-		stringToElement($$('#project_notes .description-placeholder'), 'AddADescription');
-		stringToElement($$('.sort-button .new-button-text'), 'Sort');
-		stringToElement($$('.filter-button .new-button-text'), 'Filter');
-		replaceFromElement($('pot_groupby_menu_dropdown_menu'), {
+		// replaceFromElement( $$( '.upcoming_group .group_header' ), { 'Upcoming': 'Upcoming' }); // Crashes ASANA
+		// replaceFromElement( $$( '.today_group .group_header' ), { 'Today': 'Today' }); // Crashes ASANA
+		stringToElement( $$( '.all-my-tasks-bar .label' ), 'LATER' );
+		replaceFromElement( $$( '.grid_due_date' ), days );
+		stringToElement( $$( '.grid_pseudorow_no_prioritized .grid_cell_string' ), 'AddTask' );
+		stringToElement( $$( '#archive_menu .button-text' ), 'Archive' );
+		stringToElement( $( 'archive_menu_item' ), 'ArchiveCompletedTasks' );
+		replaceFromElement( $( 'toggle_archived_menu_item' ), { 'Show archived tasks': 'ShowArchivedTasks', 'Hide archived tasks': 'HideArchivedTasks' } );
+		stringToElement( $$( '#new_menu .button-text' ), 'New' );
+		stringToElement( $( 'new_menu_item_new_task' ), 'NewTask' );
+		stringToElement( $( 'new_menu_item_new_priority_heading' ), 'NewPriorityHeading' );
+		stringToElement( $$( '#project_share_button .button-text' ), 'Share' );
+		replaceFromElement( $$( '.collapse-expand-all .button-text' ), { 'Collapse All': 'CollapseAll', 'Expand All': 'ExpandAll' } );
+		stringToElement( $$( '#group_by_priority .button-text' ), 'Priority' );
+		stringToElement( $$( '#group_by_assignee .button-text' ), 'Assignee' );
+		stringToElement( $$( '#group_by_due_date .button-text' ), 'Date' );
+		stringToElement( $$( '#group_by_project .button-text' ), 'Project' );
+		stringToElement( $$( '.filter-buttons > .toggle-button > .button-text' ), 'All' );
+		stringToElement( $$( '#project_notes .description-placeholder' ), 'AddADescription' );
+		stringToElement( $$( '.sort-button .new-button-text' ), 'Sort' );
+		stringToElement( $$( '.filter-button .new-button-text' ), 'Filter' );
+		replaceFromElement( $( 'pot_groupby_menu_dropdown_menu' ), {
 			'Priority': 'Priority',
 			'Assignee': 'Assignee',
 			'Project': 'Project',
 			'Date': 'Date'
-		});
+		} );
 
 		// Tips info
-		replaceFromElement($$('.tooltip-body'), {
+		replaceFromElement( $$( '.tooltip-body' ), {
 			'Marked for Upcoming. Click to change.': 'TIP_MarkedForUpcoming',
 			'Marked for Today. Click to change.': 'TIP_MarkedForToday',
 			'Create tasks and headings': 'TIP_CreateTasksAndHeadings',
@@ -191,23 +206,25 @@ var stringToElement = function(element, string, property){ // Quick function to 
 			'Remove Follower': 'TIP_RemoveFollower',
 			'Recently Viewed': 'TIP_RecentlyViewed',
 			'Marked as Favorite': 'TIP_MarkedFavorite'
-		});
+		} );
 
 		// Bottom buttons
-		stringToElement($$('#new_button .key-action'), 'NewTask');
-		stringToElement($$('.key-action:contains("Mark Complete")'), 'MarkComplete');
-		stringToElement($$('#delete_button .key-action'), 'DeleteTask');
-		stringToElement($$('.key-action:contains("Move Down")'), 'MoveDown');
-		stringToElement($$('.key-action:contains("Jump Down")'), 'JumpDown');
-		stringToElement($$('#close_details_button .key-action'), 'CloseDetails');
-		stringToElement($$('#quick_add_button .key-action'), 'QuickAdd');
-		stringToElement($('more_shortcuts_link'), 'More');
+		stringToElement( $$( '#new_button .key-action' ), 'NewTask' );
+		stringToElement( $$( '.key-action:contains("Mark Complete")' ), 'MarkComplete' );
+		stringToElement( $$( '#delete_button .key-action' ), 'DeleteTask' );
+		stringToElement( $$( '.key-action:contains("Move Down")' ), 'MoveDown' );
+		stringToElement( $$( '.key-action:contains("Jump Down")' ), 'JumpDown' );
+		stringToElement( $$( '#close_details_button .key-action' ), 'CloseDetails' );
+		stringToElement( $$( '#quick_add_button .key-action' ), 'QuickAdd' );
+		stringToElement( $( 'more_shortcuts_link' ), 'More' );
 
-		console.timeEnd("Asana translate:");
+		console.timeEnd( "Asana translate:" );
 
 	};
 
 // This is the request that sends the background.js
-chrome.extension.onRequest.addListener(function() {
+chrome.extension.onRequest.addListener( function () {
 	translate();
-});
+} );
+
+} ( document ) );
